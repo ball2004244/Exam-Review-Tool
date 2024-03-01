1227fa9f54d24e503ee5ca8b8c80529ac56cf063
Tam Nguyen <nguyenminhtam7124@gmail.com>
2024-02-29 19:39:20 -0500

    Working on Reveal Answer Buttons

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import json
-import pandas as pd
 import sys
-import os
-from typing import List
+from pathlib import Path
+from typing import List, Tuple
 from random import sample
+import pandas as pd
 
 
 class QuestionBank:
@@ -13,7 +13,7 @@ class QuestionBank:
 
         self.count = self.questions.shape[0]
 
-    def get_random_questions(self, num_questions: int) -> List[str]:
+    def get_random_questions(self, num_questions: int) -> List[Tuple[str, str]]:
         '''
         Randomly selects n questions from the question bank
         to build up a mock exam. The random algorithm initially
@@ -21,41 +21,111 @@ class QuestionBank:
 
         Later on, this will be replaced by a more sophisticated algorithm
         to takes into account question types and tags.
+
+        Return with pairs of random questions and answers
         '''
-        return sample(self.questions['question'].tolist(), min(num_questions, self.count))
+
+        selected_idx = sample(
+            range(self.count), min(num_questions, self.count))
+
+        selected_qa = self.questions.iloc[selected_idx]
+        return list(zip(selected_qa['question'], selected_qa['answer']))
 
 
 class Exam:
-    def __init__(self, questions: List[str]):
-        self.questions = questions
+    def __init__(self, question_pairs: List[Tuple[str, str]]) -> None:
+        '''
+        A question pair contains of 1 question & 1 answer
+        '''
+        self.question_pairs = question_pairs
 
-    def save_to_file(self, path: str):
+    def format_answer(self, answer: str) -> str:
+        '''
+        Load Markdown template for answer
+        '''
+        formatted_answer = ''
+        with open('format_answer.html', 'r') as f:
+            formatted_answer = f.read().replace('{{final_answer}}', answer)
+            
+        return formatted_answer
+
+    def display_question(self, path: str) -> None:
+        '''
+        Display the exam in a html format on the browser
+        '''
+        style = (
+            "<style>"
+            ".answer {"
+            "    display: none;"
+            "}"
+            "a {"
+            "    cursor: pointer;"
+            "}"
+            "</style>"
+        )
+
+        script = (
+            "<script type='text/javascript'>"
+            "window.onload = function() {"
+            "    var reveals = document.querySelectorAll('.reveal');"
+            "    reveals.forEach(function(reveal) {"
+            "        reveal.addEventListener('click', function() {"
+            "            console.log('clicked');"
+            "        });"
+            "    });"
+            "}"
+            "</script>"
+        )
         with open(path, 'w') as f:
-            for i, q in enumerate(self.questions):
-                f.write(f'{i+1}. {q}\n\n\n')
+            f.write(style)
+            for i, qa_pair in enumerate(self.question_pairs):
+                formatted_question = f'<br>{i+1}. {qa_pair[0]}<br>'
+                formatted_answer = self.format_answer(qa_pair[1])
+
+                f.write(f'{formatted_question}{formatted_answer}')
+            f.write('<br>')
+            f.write(script)
+            f.write('<br><br>')
+                
+    def save_to_file(self, path: str) -> None:
+        '''
+        Generate a raw text file for download
+        '''
+        with open(path, 'w') as f:
+            for i, qa_pair in enumerate(self.question_pairs):
+                f.write(f'{i+1}. {qa_pair[0]}\n\n')
+
 
 class Config:
-    def __init__(self, path: str):
+    def __init__(self, path: str) -> None:
         with open(path, 'r') as f:
             self.config = json.load(f)
 
-    def get_num_questions(self):
+    def get_num_questions(self) -> int:
         return self.config['num_questions']
 
 
 def main() -> None:
-    in_dir, out_dir = sys.argv[1], sys.argv[2]
-    question_bank_path = os.path.join(in_dir, 'question_bank.csv')
-    inp_dict_path = os.path.join(in_dir, 'variables.dictionary')
+    # Defines path
+    in_dir, out_dir = Path(sys.argv[1]), Path(sys.argv[2])
+    question_bank_path = in_dir / 'question_bank.csv'
+    inp_dict_path = in_dir / 'variables.dictionary'
+    display_file = out_dir / 'display_exam.txt'
+    download_file = out_dir / 'mock_exam.txt'
 
+    # Set up input
     question_bank = QuestionBank(question_bank_path)
     config = Config(inp_dict_path)
 
-    questions = question_bank.get_random_questions(config.get_num_questions())
-    exam = Exam(questions)
+    # Main Logic
+    question_pairs = question_bank.get_random_questions(
+        config.get_num_questions())
+    exam = Exam(question_pairs)
 
-    out_path = os.path.join(out_dir, 'mock_exam.txt')
-    exam.save_to_file(out_path)
+    # Process output
+    exam.display_question(display_file)
+    exam.save_to_file(download_file)
+    
 
 
 if __name__ == '__main__':
