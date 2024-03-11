8772a8c125d5347395bce126df9202330fb47e7b
Jack Nguyen <nguyenminhtam7124@gmail.com>
2024-03-11 18:57:18 -0400

    Add function to filter questions by tags

--- input.md
+++ input.md
@@ -4,15 +4,15 @@
 
 Our app is designed to help students prepare for exams more effectively. It gives more control to the students by allowing them to create their own exams and review them. Simply upload a csv file with the questions and answers and the app will take care of the rest.
 
-## CSV Format
+## Question Bank
+
+The question bank is a csv file with no header.
 
 The csv file should have the following format:
 
 ```
 questions, answers, tags
 ```
-
-No header is required.
 
 You can have more than 1 tag, just separate them with a semicolon. We will use the tags to filter the questions when creating an exam.
 
@@ -22,9 +22,23 @@ For example:
 What is the capital of France?, Paris, geography;capital cities
 ```
 
+## Question Tags
+
+You don't have to use tags, but they can be useful to filter the questions to generate the most relevant exam for you.
+
+For each tag, it should follow the format `tag: count`. The count is the number of questions you want to include in the exam. If you don't specify the count, we will include all the questions with that tag.
+
+Here is how you would use tags during exam creation:
+
+```
+geography: 30, calculus: 20, physics: 100
+```
+
 ## Math Notation
+
 You might want to use math notation in your questions. We support ASCII Math notation, remember to wrap your texts between 2 dollar signs `$$`. For more information about ASCII Math syntax, check out [here](https://asciimath.org/)
 
+If the mock exam does not display the math notation correctly, you can try to refresh the page or use a different browser.
 
 ## Create your own exam here
 
@@ -33,3 +47,6 @@ You might want to use math notation in your questions. We support ASCII Math not
 
 2. How many questions would you like to have?
    { num_questions }
+
+3. What question tags would you like to have? (Leave blank to include all)
+   { question_tags }
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import json
 import sys
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from random import sample
 import pandas as pd
 
@@ -12,8 +12,14 @@ class QuestionBank:
                                      'question', 'answer', 'tags'])
 
         self.count = self.questions.shape[0]
+        self.all_tags = set()
 
-    def get_random_questions(self, num_questions: int) -> List[Tuple[str, str]]:
+        for tag in self.questions['tags'].str.strip().str.split(';').explode().unique():
+            if not tag.strip():
+                continue
+            self.all_tags.add(tag.strip())
+
+    def __get_random_questions(self, num_questions: int) -> List[Tuple[str, str]]:
         '''
         Randomly selects n questions from the question bank
         to build up a mock exam. The random algorithm initially
@@ -31,6 +37,42 @@ class QuestionBank:
         selected_qa = self.questions.iloc[selected_idx]
         return list(zip(selected_qa['question'], selected_qa['answer']))
 
+    def generate_tags_questions(self, num_questions: int, tags: Dict[str, int]) -> List[Tuple[str, str]]:
+        '''
+        Generate questions based on tags.
+        If no tags are provided, then generate exam using normal random selection
+        '''
+
+        # If no tags are provided, then generate exam from all questions
+        if tags is None or len(tags) == 0:
+            return self.__get_random_questions(num_questions)
+
+        # randomly select questions based on tags
+        selected_qa = set()
+        for tag, count in tags.items():
+            if not tag in self.all_tags:
+                continue
+
+            # find all questions with the tag
+            tag_idx = self.questions[self.questions['tags'].str.contains(
+                tag)].index
+
+            # if there are not enough questions, then select all
+            if len(tag_idx) <= count:
+                selected_qa.update(tag_idx)
+                continue
+
+            # randomly select questions excluding the existed questions
+            available_idx = list(set(tag_idx) - selected_qa)
+            selected_idx = sample(
+                available_idx, min(count, len(available_idx)))
+            selected_qa.update(selected_idx)
+
+        # if there are still not enough questions, then select stop
+
+        selected_qa = self.questions.iloc[list(selected_qa)]
+        return list(zip(selected_qa['question'], selected_qa['answer']))
+
 
 class Exam:
     def __init__(self, question_pairs: List[Tuple[str, str]]) -> None:
@@ -43,7 +85,7 @@ class Exam:
         '''
         Format ASCII Math to Markdown
         '''
-        #TODO: Implement this function
+        # TODO: Implement this function
         return text
 
     def format_question(self, order: int, question: str) -> str:
@@ -51,10 +93,10 @@ class Exam:
         Load Markdown template for question
         '''
         formatted_question = f'{order}. {question}'
-        
+
         return formatted_question
 
-    def format_answer(self, id: int, answer: str, template: str='static/format_answer.html') -> str:
+    def format_answer(self, id: int, answer: str, template: str = 'static/format_answer.html') -> str:
         '''
         Load Markdown template for answer
         '''
@@ -62,23 +104,23 @@ class Exam:
         formatted_answer = ''
         with open(template, 'r') as f:
             formatted_answer = f.read()
-            
+
         formatted_answer = formatted_answer.replace('{{final_answer}}', answer)
         formatted_answer = formatted_answer.replace('{{id}}', str(id))
-        
+
         return formatted_answer
 
-    def display_question(self, path: str, header_path: str=None) -> None:
+    def display_question(self, path: str, header_path: str = None) -> None:
         '''
         Display the exam in a html format on the browser
         '''
         html_content = ''
         header = None
-        
+
         if header_path is not None:
             with open(header_path, 'r') as f:
                 header = f.read()
-                
+
         print(header)
 
         with open(path, 'w') as f:
@@ -90,7 +132,7 @@ class Exam:
                 formatted_answer = self.format_answer(i, qa_pair[1])
 
                 html_content += f'\n<br>{formatted_question}<br>\n{formatted_answer}'
-            
+
             f.write(f'{html_content}<br>')
 
     def save_to_file(self, path: str) -> None:
@@ -110,6 +152,37 @@ class Config:
     def get_num_questions(self) -> int:
         return self.config['num_questions']
 
+    def __get_raw_tags(self) -> str:
+        return self.config['question_tags']
+
+    def get_format_tags(self) -> Dict[str, int]:
+        # Convert user input string to a dictionary, where each tag is a key
+        raw_tags = self.__get_raw_tags()
+        tags_dict = {}
+        raw_tags = raw_tags.strip().split(',')  # split into pairs
+        for i, tag in enumerate(raw_tags):
+            # skip invalid tags
+            if ':' not in tag:
+                continue
+
+            # split into key value pair
+            tag, value = tag.split(':')
+
+            tag = tag.strip()
+            value = value.strip()
+
+            # skip empty tags
+            if tag == '':
+                continue
+
+            # if value is not a digit, then set it to default 1
+            if not value.isdigit():
+                value = 1
+
+            tags_dict[tag.strip()] = int(value)
+
+        return tags_dict
+
 
 def main() -> None:
     # Defines path
@@ -125,8 +198,13 @@ def main() -> None:
     config = Config(inp_dict_path)
 
     # Main Logic
-    question_pairs = question_bank.get_random_questions(
-        config.get_num_questions())
+    # This does not use tags
+    # question_pairs = question_bank.get_random_questions(
+    #     config.get_num_questions())
+
+    question_pairs = question_bank.generate_tags_questions(
+        config.get_num_questions(), config.get_format_tags())
+
     exam = Exam(question_pairs)
 
     # Process output
